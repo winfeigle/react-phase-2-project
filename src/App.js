a88518abfd2ef729e5a624e6832e093c7da14663
Win Feigle <win.feigle@gmail.com>
2022-08-21 17:23:49 -0600

    Rename Header.js to NavBar.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Route, Switch } from "react-router-dom";
 
-import Header from "./components/Header";
+import Header from "./components/NavBar";
 import Goals from "./components/Goals";
 import Accomplishments from "./components/Accomplishments";
 
