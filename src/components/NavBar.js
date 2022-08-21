793652cf91bc84a91176bf739a1ba7a773d8698b
Win Feigle <win.feigle@gmail.com>
2022-08-21 17:34:00 -0600

    Add working Routes to NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,22 +1,55 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 function NavBar() {
+
+  const linkStyles = {
+    width: "100px",
+    padding: "12px",
+    margin: "0 6px 6px",
+    background: "blue",
+    textDecoration: "none",
+    color: "white",
+  };
+
     return(
-        <header>
-          <nav>
-            <h1 className="site-title">
+        <div>
+          <h1 className="site-title">
               VISION/BOARD
-            </h1>
-            <div className="navigation">
-              <a className="button" href="/">
-                Goals
-              </a>
-              <a className="button" href="/">
-                Accomplishments
-              </a>
-            </div>
-          </nav>
-        </header>
+          </h1>
+          <NavLink
+            to="/"
+            /* set exact so it knows to only set activeStyle when route is deeply equal to link */
+            exact
+            /* add styling to Navlink */
+            style={linkStyles}
+            /* add prop for activeStyle */
+            activeStyle={{
+              background: "darkblue",
+            }}
+            >Home</NavLink>
+
+          <NavLink
+            to="/goals"
+            exact
+            style={linkStyles}
+            activeStyle={{
+              background: "darkblue",
+            }}
+            >Goals</NavLink>
+
+        <NavLink
+            to="/accomplishments"
+            exact
+            style={linkStyles}
+            activeStyle={{
+              background: "darkblue",
+            }}
+            >Accomplishments</NavLink>
+        <p>
+      
+        </p>
+        </div>
     );
 }
 
