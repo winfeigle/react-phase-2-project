3bd20bbaa7f8f9f6386dd43f00b2c2f326994b6d
Win Feigle <win.feigle@gmail.com>
2022-08-22 00:14:56 -0600

    Add project info to Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,8 +2,12 @@ import React from "react";
 
 function Home(){
     return(
-        <div>
-            Text
+        <div id="home-container">
+            <h3>Flatiron School Phase 2 Project</h3>
+            <p>Just a simple goal board app created with React...</p>
+            {'\n'}
+            <p id="author">By Win Feigle -- <a href="https://www.linkedin.com/in/edwinfeigle/">LinkedIn</a><img></img></p>
+            
         </div>
     );
 }
